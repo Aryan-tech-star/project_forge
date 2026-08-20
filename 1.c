004bcbe6ee690c32e1335a569a842758f81b8dfa
Aryan-tech-star <ARYAN.40789@stu.upes.ac.in>
2026-08-20 20:53:47 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>  
+int main ()
+{
+int a, b , sum ;
+printf("enter two integrs ") ;
+scanf("%d %d",&a ,&b) ;
+sum = a+b ;
+printf("sum of %d + %d = %d \n",a,b , sum) ; 
+return 0 ;
+}
+
