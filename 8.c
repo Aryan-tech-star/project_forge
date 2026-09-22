ea1abea9d060825abac5b7a8c120297ad3f78b0a
Aryan-tech-star <ARYAN.40789@stu.upes.ac.in>
2026-09-22 20:11:36 +0530

    Add files via upload

---
+++ 8.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+    int year;
+
+    printf("Enter year: ");
+    scanf("%d", &year);
+
+    if (year % 400 == 0)
+    {
+        printf("Leap year");
+    }
+    else if (year % 100 == 0)
+    {
+        printf("Not a leap year");
+    }
+    else if (year % 4 == 0)
+    {
+        printf("Leap year");
+    }
+    else
+    {
+        printf("Not a leap year");
+    }
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Enter a character in lowercase: ");
+    scanf("%c", &ch);
+
+if(ch == 'a' || ch == 'e' || ch == 'i' ||
+        ch == 'o' || ch == 'u')
+
+        else {
+            printf(constant)
+        }
+
+    return 0;
+}
